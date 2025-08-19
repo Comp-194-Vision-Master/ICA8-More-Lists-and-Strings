ead6ef35ccf061fdd92fef7322aa38196673fa2c
Susan Eileen Fox <fox@macalester.edu>
2025-08-18 20:54:22 -0500

    Add files via upload

---
+++ activ8.py
@@ -0,0 +1,258 @@
+""" ==============================================================================================
+File: actic8.py
+Author: Susan Fox
+Date: Fall 2025
+
+Contains functions to practice list and string operators, functions, and methods
+==================================================================================================
+"""
+
+import cv2
+import numpy as np
+
+# ===========================================================================================================
+# String operators and functions
+
+# TODO: Choose one of the functions in this section to complete
+# TODO: Keep the rest for additional practice once you complete the activity
+
+# -----------------------------------------------------------------------------------------------------------
+# symbolPattern
+
+# TODO: Put your definition of symbolPattern here
+
+
+# -----------------------------------------------------------------------------------------------------------
+# firstVowel
+
+# TODO: Put your definition of firstVowel here
+
+
+# ===========================================================================================================
+# List operators and functions
+
+# TODO: Choose one of the functions in this section to complete
+# TODO: Keep the rest for additional practice once you complete the activity
+
+
+# -----------------------------------------------------------------------------------------------------------
+# everyOther
+
+# TODO: Put your definition of everyOther here
+
+
+# -----------------------------------------------------------------------------------------------------------
+# sumPositive
+
+# TODO: Put your definition of sumPositive here
+
+
+# ===========================================================================================================
+# Modifying lists
+
+# -----------------------------------------------------------------------------------------------------------
+# changeStart
+
+# TODO: Put your definition of changeStart here
+
+
+# ===========================================================================================================
+# String methods
+
+# TODO: Choose one of the functions in this section to complete
+# TODO: Keep the rest for additional practice once you complete the activity
+
+
+# -----------------------------------------------------------------------------------------------------------
+# shout
+
+# TODO: Put your definition of shout here
+
+
+# -----------------------------------------------------------------------------------------------------------
+# nameSubst
+
+# TODO: Put your definition of nameSubst here
+
+
+# -----------------------------------------------------------------------------------------------------------
+# countWords (optional challenge)
+
+# TODO: Put your definition of countWords here
+
+
+# ===========================================================================================================
+# List methods
+
+
+# -----------------------------------------------------------------------------------------------------------
+# onBeyond
+
+# TODO: Put your definition of onBeyond here
+
+
+# ===========================================================================================================
+# Tuples
+
+
+# -----------------------------------------------------------------------------------------------------------
+# endPoints
+
+# TODO: Put your definition of endPoints here
+
+
+# ===========================================================================================================
+# List comprehensions
+
+# TODO: Choose one of the functions in this section to complete
+# TODO: Keep the rest for additional practice once you complete the activity
+
+# -----------------------------------------------------------------------------------------------------------
+# timesN
+
+# TODO: Put your definition of timesN here
+
+
+# -----------------------------------------------------------------------------------------------------------
+# removeAll
+
+# TODO: Put your definition of removeAll here
+
+# ===========================================================================================================
+# Optional Challenge Activities
+
+# TODO: these are completely optional and harder problems for those of you up for a challenge
+# TODO: Put your definitions of any/all functions from this section here.
+
+
+
+
+# -----------------------------------------------------------------------------------------------------------
+# Main script
+
+if __name__ == "__main__":
+    # -----------------------------------------------
+    # Test calls for symbolPattern
+    # print("Testing symbolPattern")
+    # print(symbolPattern("*!*!", 5))
+    # print(symbolPattern("@$&", 3))
+    # print(symbolPattern("(*)", 4))
+
+    # TODO: Add more calls to symbolPattern here
+
+
+    # -----------------------------------------------
+    # Test calls for firstVowel
+    # print("Testing firstVowel")
+    # print(firstVowel("strings"))
+    # print(firstVowel("myth busters"))
+    # print(firstVowel("equal"))
+
+    # TODO: Add more calls to firstVowel here
+
+
+    # -----------------------------------------------
+    # Test calls for everyOther
+    # print("Testing everyOther")
+    # print(everyOther([1, 2, 3, 4, 5]))
+    # print(everyOther(['d', 'e', 'f', 'g']))
+    # print(everyOther([]))
+
+    # TODO: Add more calls to everyOther here
+
+
+    # -----------------------------------------------
+    # Test calls for sumPositive
+    # print("Testing sumPositive")
+    # print(sumPositive([-1, -3, 4, 2, -6, 6]))
+    # print(sumPositive([-10, -20, -30]))
+    # print(sumPositive([1, 1, 1, 1, 1, 1]))
+    # print(sumPositive([]))
+
+    # TODO: Add more calls to sumPositive here
+
+
+    # -----------------------------------------------
+    # Test calls for changeStart
+    # print("Testing changeStart")
+    # tList = ['a', 'b', 'c', 'd', 'e', 'f']
+    # changeStart('X', tList)
+    # print(tList)
+
+    # TODO: Add more calls to changeStart here
+
+
+    # -----------------------------------------------
+    # Test calls for shout
+    # print("Testing shout")
+    # newS = shout("What are you doing?")
+    # print(newS)
+    # print(shout("i saw a frog in my bathtub"))
+
+    # TODO: Add more calls to shout here
+
+
+    # -----------------------------------------------
+    # Test calls for nameSubst
+    # print("Testing nameSubst")
+    # sallie = nameSubst("Sallie", "My friend, ZZZ, won an award.")
+    # print(sallie)
+    # print(nameSubst("Fred", "Jamie and ZZZ flew over the trees."))
+
+    # TODO: Add more calls to nameSubst here
+
+
+    # -----------------------------------------------
+    # Test calls for countWords
+    # print("Testing countWords")
+    # count1 = countWords("ban", "I ban bananas, but I don't ban peaches")
+    # print(count1)
+    # print(countWords("aba", "abaa bba ab aba bbb aba baba aba ba ba ab a aaabbaa"))
+
+    # TODO: Add more calls to countWords here
+
+    # -----------------------------------------------
+    # Test calls for onBeyond
+    # print("Testing onBeyond")
+    # nl1 = [1, 3, 5, 6, 9]
+    # nl2 = [-2.5, 0, 1.372, 4.7]
+    # nl3 = [15.2]
+    # print("Before:")
+    # print(nl1)
+    # print(nl2)
+    # print(nl3)
+    # onBeyond(15, nl1)
+    # onBeyond(7, nl2)
+    # onBeyond(20, nl3)
+    # print("After:")
+    # print(nl1)
+    # print(nl2)
+    # print(nl3)
+
+    # TODO: Add more calls to onBeyond here
+
+
+    # -----------------------------------------------
+    # Test calls for endPoints
+    # print("Testing endPoints")
+    # ends = endPoints([5, 2, 12, 1, 8, 9])
+    # print(ends)
+    # lo, hi = endPoints([25, 10, 50, 45, 5])
+    # print(lo, hi)
+
+    # TODO: Add more calls to endPoints here
+
+
+    # -----------------------------------------------
+    # Test calls for timesN
+    # print("Testing timesN")
+    # TODO: Add calls to timesN here
+
+
+    # -----------------------------------------------
+    # Test calls for removeAll
+    # print("Testing removeAll")
+    # TODO: Add calls to removeAll here
+
+    # -----------------------------------------------
+    # TODO: Add calls to optional challenge activities here
